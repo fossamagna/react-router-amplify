9ad223356babfdca142ab7a4f3c76e5d8faff9e7
MURAKAMI Masahiko <fossamagna2@gmail.com>
2025-04-18 15:49:07 +0900

    feat: add signOut button

--- examples/todo-app/app/routes/index.tsx
+++ examples/todo-app/app/routes/index.tsx
@@ -1,8 +1,9 @@
 import type { Route } from "./+types/index";
 import { TodoList } from "~/components/TodoList";
 import { runWithAmplifyServerContext } from "~/lib/amplifyServerUtils";
-import { data, Link } from "react-router";
+import { data, Link, useNavigate } from "react-router";
 import { client } from "~/lib/amplify-ssr-client";
+import { signOut } from "aws-amplify/auth";
 
 export function meta() {
   return [
@@ -30,18 +31,31 @@ export async function loader({ request }: Route.LoaderArgs) {
 
 export default function Home({ loaderData }: Route.ComponentProps) {
   const { todos } = loaderData;
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col gap-y-3 my-4 mx-2">
       <h1 className="text-2xl font-bold">Todo List</h1>
       <TodoList items={todos} />
-      <Link to="/new">
+      <div className="flex justify-between">
+        <Link to="/new">
+          <button
+            type="button"
+            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+          >
+            Add Todo
+          </button>
+        </Link>
         <button
           type="button"
-          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+          className="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded"
+          onClick={async () => {
+            await signOut();
+            await navigate("/login");
+          }}
         >
-          Add Todo
+          SignOut
         </button>
-      </Link>
+      </div>
     </div>
   );
 }
