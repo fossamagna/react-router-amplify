a615078510ce2c48203b4f092dc15dc65784842d
MURAKAMI Masahiko <fossamagna2@gmail.com>
2025-06-16 22:37:00 +0900

    test: use 'projects' instead of 'workspace' in vitest config (#98)

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    workspace: ["packages/*/vitest.config.{unit,integration}.ts"],
+    projects: ["packages/*/vitest.config.{unit,integration}.ts"],
     coverage: {
       exclude: [
         "coverage/**",
